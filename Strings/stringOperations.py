998d0d7c6b09b766e5a61d386de29bb874cd8192
Karolina Redden <dubajkaro@gmail.com>
2023-03-07 13:53:05 -0400

    string concating

---
+++ Strings/stringOperations.py
@@ -0,0 +1,22 @@
+#concating
+
+material = "snow"
+shape = "ball"
+projectile = material + shape
+print(projectile)
+
+firstName = "Stan"
+lastName = "Petrov"
+fullName = firstName + lastName
+fullName = firstName + " " + lastName
+print(fullName)
+
+savory = "pb"
+sweet = "jam"
+sandwich = savory + " and " + sweet
+print(sandwich)
+
+step1 = "Eat\n"
+step2= "Sleep\n"
+step3 = "Code\n"
+print(step1+step2+step3)
