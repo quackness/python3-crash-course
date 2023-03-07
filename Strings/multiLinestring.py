f3cfbb04c06773273c0c55f55f462bfb5c6192b3
Karolina Redden <dubajkaro@gmail.com>
2023-03-07 13:45:01 -0400

    add a new line with \n and triple quotes

---
+++ Strings/multiLinestring.py
@@ -0,0 +1,10 @@
+"add \n to create linebreaks"
+prettyValidstring = "Hey Jude\nDon't make it bad\nTake a sad song\nAnd make it better"
+print(prettyValidstring)
+
+#triple ""
+prettyValidstring = """Hey Jude
+Don't make it bad
+Take a sad song
+And make it better"""
+print(prettyValidstring)
