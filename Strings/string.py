54928b5ebfbf8c0dad1888620efb8c30fe118f5f
Karolina Redden <dubajkaro@gmail.com>
2023-03-07 12:12:00 -0400

    practise escape chars

---
+++ Strings/string.py
@@ -0,0 +1,15 @@
+# print('Let's')
+print("Let's'")
+print('I said, "we should.." and left')
+# print("I said, "we should.." and left")
+#escape "" or ''
+print("She asked, \"can't we?\" if only.")
+print('She asked,"can\'t we?" if only.')
+
+#other
+print('She asked \n test') #newline
+print('She asked \a test') #Bell (BEL)
+print('She asked \t test') #Horizontal Tab
+print('She asked \v test') #Vertical Tab (VT)
+
+
