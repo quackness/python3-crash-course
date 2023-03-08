adbf3e205d3cd77438b1f8277e53feceaf63f4d1
Karolina Redden <dubajkaro@gmail.com>
2023-03-08 12:05:59 -0400

    test basic math calculations with Python3

---
+++ Numbers/math.py
@@ -0,0 +1,27 @@
+a = 14
+b = 5
+
+#addition 
+print(a + b)
+
+#subtraction
+
+print(a - b)
+
+#mult
+print(a * b)
+
+#division
+print(a / b)
+
+#integer divistion, rounds up so there is no decimal 
+print(a // b)
+
+#exponentiation a to the power of b
+print(a ** b)
+
+#compound calc.
+c = 8
+print(a + b * c ** a)
+
+#math library https://docs.python.org/3/library/math.html
